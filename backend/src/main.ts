da436433d3f1bca840dfb349ba5b862563673b9b
sharjeel7911 <sharjeelahmad7911@gmail.com>
2026-08-12 23:27:05 +0500

    Initialized the project. Check README.md for more info.

---
+++ backend/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3001);
+
+  console.log(`Vendor Portal Backend running on http://localhost:3001`);
+}
+bootstrap();
